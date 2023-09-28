bdda6a5ad64f2016359ae17a2003e6c4a8e7af72
William Oliveira <wilpo93.wo@gmail.com>
2023-09-27 23:43:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// Exemplo simples de JavaScript
+
 document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault(); // Evitar o envio padrão do formulário
-    alert("Formulário enviado com sucesso!");
+
+    // Redirecionar para a página inicial após o envio do formulário
+    window.location.href = "index.html?enviado=1";
 });
